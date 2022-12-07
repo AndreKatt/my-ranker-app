4e74bfcef90fad6346cfaf5b46ede140fdd5ba9b
Ekaterina Andreeva <andreevaekatt@gmail.com>
2022-12-07 18:37:55 +0300

    add auth guard

--- server/src/polls/polls.contoller.ts
+++ server/src/polls/polls.contoller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { ControllerAuthGuard } from './controller-auth.guard';
 import { CreatePollDto, JoinPollDto } from './dtos';
 import { PollsService } from './polls.service';
+import { RequestAuth } from './types';
 
 @Controller('polls')
 export class PollsController {
@@ -20,12 +22,14 @@ export class PollsController {
     return result;
   }
 
+  @UseGuards(ControllerAuthGuard)
   @Post('/rejoin')
-  async rejoin() {
+  async rejoin(@Req() request: RequestAuth) {
+    const { userID, pollID, name } = request;
     const result = await this.pollsService.rejoinPoll({
-      name: 'From token',
-      pollID: 'Also from token',
-      userID: 'From a token',
+      name,
+      pollID,
+      userID,
     });
 
     return result;
